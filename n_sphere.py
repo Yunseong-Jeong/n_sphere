e546e978c24ae90e00c7efd4bdf23510a821a18e
Yunseong <yunseong14@naver.com>
2019-12-04 15:29:18 +0900

    1-dimension fixed

--- n_sphere.py
+++ n_sphere.py
@@ -3,50 +3,42 @@
 import numpy as np
 import math
 import torch
+
 def convert_spherical(input, digits= None):
     if(not(
             type(input).__name__ == 'Tensor'
         or  type(input).__name__ == 'ndarray'
         or  type(input).__name__ == 'float'
         or  type(input).__name__ == 'int'
+        or  type(input).__name__ == 'list'
     )):
         print("Input Error")
         return -1
-
     # Check Numpy or list
     if digits is None:
         digits = 6 # Fixed Digits But if you want to change value, It can be helpful
     result =[]
-    dimension=[]
-    pointer = input
-    while(True):
-        if(len(pointer)):
-            dimension.append(len(pointer))
-            pointer = pointer[0]
-        else:
-            break
+    input_type = type(input).__name__
+    if(input_type == 'list'):
+        input = np.array(input)
     #over 2-dimension (current available 2-dimension)
-    if(len(dimension)==1):
+    if(input.ndim==1):
         r = 0
-        for i in range(0,dimension[0]):
+        for i in range(0,len(input)):
             r += input[i]*input[i]
         r=math.sqrt(r)
         convert = [r]
-        for i in range(0, dimension[0]-2):
+        for i in range(0, len(input)-2):
             convert.append(round(math.acos(input[i]/r),digits))
             r=math.sqrt(r*r - input[i]*input[i])
         if(input[-2]>=0):
             convert.append(math.acos(input[-2]/r))
         else:
             convert.append(2*math.pi - math.acos(input[-2]/r))
-        if(type(input).__name__!= 'list'):
-            convert = np.array(convert)
-            if(type(input).__name__=='Tensor'):
-                convert = torch.from_numpy(convert)
-        result += [convert]
-        if(type(input).__name__=='ndarray'):
+        result = convert
+        if(input_type=='ndarray'):
             result = np.array(result)
-        elif(type(input).__name__ == 'Tensor'):
+        elif(input_type== 'Tensor'):
             result = torch.stack(result)
     else:
         for element in range(0, len(input)):
@@ -79,6 +71,7 @@ def convert_rectangular(input,digits=None):
             or type(input).__name__ == 'ndarray'
             or type(input).__name__ == 'float'
             or type(input).__name__ == 'int'
+            or type(input).__name__ == 'list'
     )):
         print("Input Error")
         return -1
@@ -86,30 +79,46 @@ def convert_rectangular(input,digits=None):
     if digits is None:
         digits = 6
     result = []
-    #over 2-dimension
-    for element in range (0, len(input)):
-        r = input[element][0]
+    input_type = type(input).__name__
+    if(input_type=='list'):
+        input = np.array(input)
+    if(input.ndim==1):
+        r = input[0]
         multi_sin = 1
-        convert =[]
-        for i in range(1, len(input[element])-1):
-            #convert.append(round(r*multi_sin*math.cos(input[element][i]),digits))
-            convert.append(r * multi_sin * math.cos(input[element][i]))
-            multi_sin *= math.sin(input[element][i])
-        #convert.append(round(r*multi_sin*math.cos(input[element][-1]),digits))
-        #convert.append(round(r*multi_sin*math.sin(input[element][-1]),digits))
-        convert.append(r*multi_sin*math.cos(input[element][-1]))
-        convert.append(r*multi_sin*math.sin(input[element][-1]))
-        if (type(input).__name__ != 'list'):  # input == Numpy or Tensor
+        convert=[]
+        for i in range(1, len(input)-1):
+            convert.append(r* multi_sin * math.cos(input[i]))
+            multi_sin *= math.sin(input[i])
+        convert.append(r*multi_sin*math.cos(input[-1]))
+        convert.append(r*multi_sin*math.sin(input[-1]))
+        if(input_type != 'list'):
             convert = np.array(convert)
-            if (type(input).__name__ == 'Tensor'):
+            if(input_type=='Tensor'):
                 convert = torch.from_numpy(convert)
-        print(convert)
-        result +=[convert]
-        print(result)
-    if (type(input).__name__ == 'ndarray'):
-        result = np.array(result)
-    elif (type(input).__name__ == 'Tensor'):
-        result = torch.stack(result)
+        result = convert
+    else:
+        #over 2-dimension
+        for element in range (0, len(input)):
+            r = input[element][0]
+            multi_sin = 1
+            convert =[]
+            for i in range(1, len(input[element])-1):
+                #convert.append(round(r*multi_sin*math.cos(input[element][i]),digits))
+                convert.append(r * multi_sin * math.cos(input[element][i]))
+                multi_sin *= math.sin(input[element][i])
+            #convert.append(round(r*multi_sin*math.cos(input[element][-1]),digits))
+            #convert.append(round(r*multi_sin*math.sin(input[element][-1]),digits))
+            convert.append(r*multi_sin*math.cos(input[element][-1]))
+            convert.append(r*multi_sin*math.sin(input[element][-1]))
+            if (type(input).__name__ != 'list'):  # input == Numpy or Tensor
+                convert = np.array(convert)
+                if (type(input).__name__ == 'Tensor'):
+                    convert = torch.from_numpy(convert)
+            result +=[convert]
+        if (type(input).__name__ == 'ndarray'):
+            result = np.array(result)
+        elif (type(input).__name__ == 'Tensor'):
+            result = torch.stack(result)
     return result
 
 #### Temporary Not Use
